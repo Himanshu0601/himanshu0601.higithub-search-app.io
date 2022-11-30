eae12eaafe785ca920765ed797bce9b3666f3259
Himanshu <himanshu06012001@gmail.com>
2022-11-30 18:20:42 +0530

    completed project

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Info, User, Search, Navbar } from '../components';
+const Dashboard = () => {
+  return (
+    <main>
+      <Navbar></Navbar>
+      <Search></Search>
+      <Info></Info>
+      <User></User>
+    </main>
+  );
+};
+
+export default Dashboard;
